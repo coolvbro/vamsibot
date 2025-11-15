44a861cda8ec8c4facd865ac2d41725b7c9b7948
coolvbro <javitharava143@gmail.com>
2025-11-15 23:46:07 +0530

    Create tweet.py

---
+++ tweet.py
@@ -0,0 +1,17 @@
+import tweepy
+import os
+
+consumer_key = os.environ['TWITTER_API_KEY']
+consumer_secret = os.environ['TWITTER_API_SECRET_KEY']
+access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
+
+client.create_tweet(text="vamsi erripuku")
+print("Tweet sent!")
